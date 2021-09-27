f96388f6d6ef3c197fa428109c3a204aac74b7c9
lais amorim moura <ladmoura3800@gmail.com>
2021-09-27 14:02:31 -0400

    criacao do projeto

---
+++ src/main/java/br/edu/ifms/bookflix/BookflixApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifms.bookflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookflixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifms/bookflix/BookflixApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifms.bookflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
